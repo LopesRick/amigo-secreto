67252520ec31bcf25dd342af08f4367f9ecc6abf
Rick Lopes <franciscohenrique22@live.com>
2025-09-02 22:59:24 -0300

    Adicionando a função sorteio

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ let listaDeNomes = [];
 
 function adicionarAmigo() {
     let nomeAmigo = document.getElementById('amigo').value;
-    if (nomeAmigo !== '') {
-        listaDeNomes.push(nomeAmigo);
-        document.getElementById('amigo').value = '';
-        document.getElementById('listaAmigos').innerText = listaDeNomes.join(', ');
+    if (nomeAmigo === '') {
+        alert("Digite um nome:");
+        return;
     }
+    if (listaDeNomes.includes(nomeAmigo)) {
+        alert("O nome do amigo já foi adicionado!");
+         document.getElementById('amigo').value = '';
+         return;
+    }
+    listaDeNomes.push(nomeAmigo);
+    document.getElementById('amigo').value = '';
+    document.getElementById('listaAmigos').innerText = listaDeNomes.join(', ');
 }
 
+/*function verificarSorteio() {
+    if (listaDeNomes.length < 2) {
+        alert("Adicione pelo menos dois nomes para sortear.");
+        return;
+    }
+}*/ //Revisar depois
+
 function sortearAmigo() {
-    let sorteiaNomes = Math.floor(Math.random() * listaDeNomes.length);
-    return listaDeNomes[sorteiaNomes];
+    let nomeSorteado = Math.floor(Math.random() * listaDeNomes.length);
+    let amigoSorteado = listaDeNomes[nomeSorteado];
+    document.getElementById('resultado').innerText = `O amigo sorteado foi: ${amigoSorteado}`;
 }
